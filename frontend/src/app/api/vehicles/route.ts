48891af5f288723a4fea81ad0083aaaaf44d7a89
RAJI-Zakaria <zakariaraji.me@gmail.com>
2025-01-11 13:46:55 +0100

    adding Vehicle filterby year, type & manufacturer

--- frontend/src/app/api/vehicles/route.ts
+++ frontend/src/app/api/vehicles/route.ts
@@ -1,11 +1,34 @@
 // Here we will add the route to get all vehicles using the filter
 import { NextRequest, NextResponse } from "next/server";
-import Vehicles from "@/dump/vehicles.json";
+import data from "@/dump/vehicles.json";
+import { Vehicle } from "@/types";
+
+const vehicles = data as Vehicle[];
 
 export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+
+  // Getting Filter Parameters
+  const manufacturer = searchParams.get("manufacturer");
+  const type = searchParams.get("type");
+  const year = searchParams.get("year");
+
+  const filteredVehicles = vehicles.filter((vehicle: Vehicle) => {
+    if (manufacturer && vehicle.manufacturer !== manufacturer) {
+      return false;
+    }
+    if (type && vehicle.type !== type) {
+      return false;
+    }
+    if (year && vehicle.year !== parseInt(year)) {
+      return false;
+    }
+    return true;
+  });
+
   return NextResponse.json(
     {
-      Vehicles,
+      filteredVehicles,
     },
     { status: 200 }
   );
