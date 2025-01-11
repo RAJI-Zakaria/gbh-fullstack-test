71dfe603ff8f935add61c92b93bdd58eb97ee693
RAJI-Zakaria <zakariaraji.me@gmail.com>
2025-01-11 13:36:53 +0100

    adding dump/vehicles.json and endpoint to fetch all vehicles

--- frontend/src/app/api/vehicles/route.ts
+++ frontend/src/app/api/vehicles/route.ts
@@ -1,1 +1,12 @@
-// here we will add the route to get all vehicles using the filter
+// Here we will add the route to get all vehicles using the filter
+import { NextRequest, NextResponse } from "next/server";
+import Vehicles from "@/dump/vehicles.json";
+
+export async function GET(request: NextRequest) {
+  return NextResponse.json(
+    {
+      Vehicles,
+    },
+    { status: 200 }
+  );
+}
