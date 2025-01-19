e113211a4ad2c328cc009a075a9f93f04fbf732f
RAJI-Zakaria <zakariaraji.me@gmail.com>
2025-01-19 02:39:50 +0100

    fixing bug - deploy

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,7 +20,7 @@ const HomePage = async () => {
 
   return (
     <div className="space-y-3">
-      {BASE_URL}
+      BASE_URL{BASE_URL}
       <Hero />
       <h1 className="text-3xl uppercase text-center mb-10">
         Most Affordable Cars
